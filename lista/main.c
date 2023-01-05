f348fef1612f49d71b4eabc7b9c293173c373fbb
viniciusddrft <viniciusddrft5@gmail.com>
2023-01-05 18:32:32 -0300

    arvore binaria

--- lista/main.c
+++ lista/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include "list.h"
 
-int main(){
+int main()
+{
     addFirst(2, 2);
     addFirst(5, 5);
     addFirst(8, 8);
-    addLast(11, 11);
-    addIndex(13, 13, 1);
-    removeIndex(0);
-    removeIndex(3);
+
     imprime();
     printf("\nNumero total te items -> %d \n", length());
 
